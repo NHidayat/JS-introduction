8f41af6a2d59c41d602b912045eae75ec881bae5
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-07-26 18:57:56 +0700

    clean

--- tugas2.js
+++ tugas2.js
@@ -1,7 +1,7 @@
-const mtk = 10
+const mtk = 90
 const bahasaIndonesia = 100
 const bahasaInggris = 89
-const ipa = 69
+const ipa = 90
 
 if (typeof mtk != 'number' || typeof bahasaIndonesia != 'number' || typeof bahasaInggris != 'number' || typeof ipa != 'number' ) {
 
@@ -12,8 +12,7 @@ if (typeof mtk != 'number' || typeof bahasaIndonesia != 'number' || typeof bahas
     let rata = (mtk+bahasaIndonesia+bahasaInggris+ipa)/4
     
     rata >= 90 && rata <= 100 ?
-    console.log(`
-    Nilai = ${rata} \nGrade = A`):
+    console.log(`Nilai = ${rata} \nGrade = A`):
     
     rata >= 80 && rata <= 89 ?
     console.log(`Nilai = ${rata} \nGrade = B`):
